ab519da17a463a90dc3f60a8d382e8ee5e8f344d
Neha <nehashingade1004@gmail.com>
2026-02-17 12:30:28 +0530

    Updated Etransfer screen

--- src/Payments/Etransfer.js
+++ src/Payments/Etransfer.js
@@ -3,32 +3,77 @@ import {
   Text,
   TouchableOpacity,
   StyleSheet,
-  TextInput
+  TextInput,
+  Alert
 } from 'react-native'
-import React from 'react'
-import { useTheme } from '../theme/theme-context'
-import Icon from 'react-native-vector-icons/Ionicons'
-import { useTheme } from '../theme/theme-context'
+import React, { useContext, useState } from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import { ThemeContext } from '../Theme/ThemeContext'
 
 const Etransfer = ({ navigation }) => {
 
-  const { theme } = useTheme()
+  const [amount, setAmount] = useState("");
+  const [sendTo, setSendTo] = useState("");
+  const [note, setNote] = useState("");
+
+  const [amountError, setAmountError] = useState("");
+  const [sendToError, setSendToError] = useState("");
+  const [noteError, setNoteError] = useState("");
+
+  const { theme } = useContext(ThemeContext)
+
+  const sendMoney = () => {
+    let valid = true;
+
+    if(!amount) {
+      setAmountError('This field is required');
+      valid = false
+    } else {
+      setAmountError('')
+    }
+    if(!sendTo) {
+      setSendToError('This field is required')
+      valid = false
+    } else {
+      setSendToError('')
+    }
+    if (!note) {
+      setNoteError('his field is required')
+      valid = false
+    } else {
+      setNoteError('')
+    }
+    if (!valid) return;
+
+        Alert.alert("Send Money Successfully");
+
+        setAmount("");
+  setSendTo("");
+  setNote("");
+
+  }
 
   return (
     <View style={[styles.container, { backgroundColor: theme.background }]}>
 
       <TouchableOpacity
-        style={styles.header}
-        onPress={() => navigation.navigate('PaymentMethod')}
-      >
-        <Icon name="close" size={30} color={theme.primary} />
+              style={styles.header}
+              onPress={() => navigation.navigate('PaymentMethod')
+              }
+            >
+              <Icon
+                name="close"
+                size={28}
+                color={"red"}
+                style={{ borderWidth: 2, borderColor: theme.primary, padding: 4 }}
+              />
+              <Text style={[styles.headerText, { color: theme.primary }]}>
+                E - Transfer
+              </Text>
+            </TouchableOpacity>
 
-        <Text style={[styles.headerText, { color: theme.primary }]}>
-          E-Transfer
-        </Text>
-      </TouchableOpacity>
-
-      <View style={styles.inputContainer}>
+      <View style={[styles.inputContainer, ]}>
         <Text style={[styles.label, { color: theme.text }]}>
           Enter Amount
         </Text>
@@ -39,15 +84,26 @@ const Etransfer = ({ navigation }) => {
             {
               borderColor: theme.primary,
               color: theme.text
-            }
+            },
+            amountError ? styles.errorInput : null
           ]}
           placeholder="3500"
           placeholderTextColor={theme.text}
           keyboardType="number-pad"
+          onChangeText={(text) => {
+            setAmount(text)
+            setAmountError('')
+          }}
+          value={amount}
         />
+        {amountError ? (
+        <Text style={styles.errorText}>{amountError}</Text>
+      ) : null}
       </View>
 
-      <View style={styles.inputContainer}>
+      
+
+      <View style={[styles.inputContainer, ]}>
         <Text style={[styles.label, { color: theme.text }]}>
           Send To
         </Text>
@@ -58,14 +114,26 @@ const Etransfer = ({ navigation }) => {
             {
               borderColor: theme.primary,
               color: theme.text
-            }
+            },
+            sendToError ? styles.errorInput : null
+            
           ]}
           placeholder="+1(xxx)-xxx-xxxx"
           placeholderTextColor={theme.text}
+          keyboardType="number-pad"
+          onChangeText={(text) => {
+            setSendTo(text)
+            setSendToError('')
+          }}
+          value={sendTo}
+          
         />
+        {sendToError ? (
+        <Text style={styles.errorText}>{sendToError}</Text>
+      ) : null}
       </View>
 
-      <View style={styles.inputContainer}>
+      <View style={[styles.inputContainer, ]}>
         <Text style={[styles.label, { color: theme.text }]}>
           Add a Note
         </Text>
@@ -76,11 +144,20 @@ const Etransfer = ({ navigation }) => {
             {
               borderColor: theme.primary,
               color: theme.text
-            }
+            },
+            noteError ? styles.errorInput : null
           ]}
           placeholder="Enter a note"
           placeholderTextColor={theme.text}
+          onChangeText={(text) => {
+            setNote(text)
+            setNoteError('')
+          }}
+          value={note}
         />
+        {noteError ? (
+        <Text style={styles.errorText}>{noteError}</Text>
+      ) : null}
       </View>
 
       <TouchableOpacity
@@ -88,6 +165,7 @@ const Etransfer = ({ navigation }) => {
           styles.sendBtn,
           { backgroundColor: theme.primary }
         ]}
+        onPress={sendMoney}
       >
         <Text style={styles.sendText}>
           Send Money
@@ -111,7 +189,7 @@ const styles = StyleSheet.create({
   },
   headerText: {
     flex: 1,
-    paddingHorizontal: 70,
+    paddingHorizontal: 50,
     fontSize: 30,
     fontWeight: '700',
   },
@@ -141,6 +219,15 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '700',
     textAlign: 'center',
+  },
+  errorInput: {
+    borderColor: 'red',
+  },
+
+  errorText: {
+    color: 'red',
+    marginTop: 4,
+    fontSize: 13,
   },
 })
 
